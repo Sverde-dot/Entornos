8678e5b6ce47ada15ee9d973994f1407596bd5da
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-10-23 13:30:53 +0200

    Cambio

---
+++ src/COD/COD.java
@@ -0,0 +1,11 @@
+
+package COD;
+
+public class COD {
+
+    public static void main(String[] args) {
+        System.out.println("COD es call of duty");
+        System.out.println("COD es Contornos de Desenvolvemento");
+    }
+    
+}
