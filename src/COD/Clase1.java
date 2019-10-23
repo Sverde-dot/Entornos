8ae279cd77e2ff10f03d8822a14e6306186c89ca
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-10-23 14:02:39 +0200

    Nueva Clase!

---
+++ src/COD/Clase1.java
@@ -0,0 +1,14 @@
+
+package COD;
+//@author irodriguezsteuerberg
+
+public class Clase1 {
+private int numero;
+public void setNumero(int numero){
+this.numero=numero;    
+}
+public int getNumero(){
+return numero;    
+}
+
+}
